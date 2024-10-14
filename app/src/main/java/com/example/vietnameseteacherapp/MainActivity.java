792ddd752b42cb0c0cf653681d21c4ddcc320d6b
Tan Dao <ttdao3@asu.edu>
2024-10-13 23:35:48 -0700

    add voice

--- app/src/main/java/com/example/vietnameseteacherapp/MainActivity.java
+++ app/src/main/java/com/example/vietnameseteacherapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.vietnameseteacherapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button blackBtn,greenBtn,purpleBtn,redBtn,yellowBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,74 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        blackBtn = findViewById(R.id.blackBtn);
+        greenBtn = findViewById(R.id.greenBtn);
+        purpleBtn = findViewById(R.id.purpleBtn);
+        redBtn = findViewById(R.id.redBtn);
+        yellowBtn = findViewById(R.id.yellowBtn);
+
+        yellowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.mau_vang
+                );
+                mediaPlayer.start();
+
+            }
+        });
+
+        purpleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.mau_tim
+                );
+                mediaPlayer.start();
+
+            }
+        });
+
+        greenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.mau_xanh
+                );
+                mediaPlayer.start();
+
+            }
+        });
+
+        redBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.mau_do
+                );
+                mediaPlayer.start();
+
+            }
+        });
+
+        blackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.mau_den
+                );
+                mediaPlayer.start();
+
+            }
+        });
+
+
+
     }
 }
