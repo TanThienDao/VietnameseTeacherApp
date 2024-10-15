1f699c2299d480f673d0dbb911d2942a88874e48
Tan Dao <ttdao3@asu.edu>
2024-10-14 22:43:38 -0700

    handle multiple click

--- app/src/main/java/com/example/vietnameseteacherapp/MainActivity.java
+++ app/src/main/java/com/example/vietnameseteacherapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button blackBtn,greenBtn,purpleBtn,redBtn,yellowBtn;
     @Override
@@ -31,67 +32,39 @@ public class MainActivity extends AppCompatActivity {
         redBtn = findViewById(R.id.redBtn);
         yellowBtn = findViewById(R.id.yellowBtn);
 
-        yellowBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(
-                        getApplicationContext(),
-                        R.raw.mau_vang
-                );
-                mediaPlayer.start();
+        yellowBtn.setOnClickListener(this);
+        purpleBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+        redBtn.setOnClickListener(this);
+        blackBtn.setOnClickListener(this);
+    }
 
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        // Find the button by ID and play the correct sound
+        int clickBtnId = v.getId();
 
-        purpleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(
-                        getApplicationContext(),
-                        R.raw.mau_tim
-                );
-                mediaPlayer.start();
+        if(clickBtnId == R.id.blackBtn) {
+            playSound(R.raw.mau_den);
+        }else if(clickBtnId == R.id.greenBtn){
+            playSound(R.raw.mau_xanh);
+        }else if(clickBtnId == R.id.purpleBtn){
+            playSound(R.raw.mau_tim);
+        }else if(clickBtnId == R.id.redBtn){
+            playSound(R.raw.mau_do);
+        }else if(clickBtnId == R.id.yellowBtn){
+            playSound(R.raw.mau_vang);
+        }else {
+            Toast.makeText(this,"Error !",Toast.LENGTH_SHORT).show();
+        }
 
-            }
-        });
-
-        greenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(
-                        getApplicationContext(),
-                        R.raw.mau_xanh
-                );
-                mediaPlayer.start();
-
-            }
-        });
-
-        redBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(
-                        getApplicationContext(),
-                        R.raw.mau_do
-                );
-                mediaPlayer.start();
-
-            }
-        });
-
-        blackBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(
-                        getApplicationContext(),
-                        R.raw.mau_den
-                );
-                mediaPlayer.start();
-
-            }
-        });
-
-
+    }
+    public void playSound(int id){
+        MediaPlayer mediaPlayer = MediaPlayer.create(
+                this,
+                id
+        );
+        mediaPlayer.start();
 
     }
 }
